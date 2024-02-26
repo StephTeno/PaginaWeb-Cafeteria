853c0b1bdaaf1830bda77de248c6b54d9a26fa0d
StephTeno <StephanieTenorioCarolinaOrozco@gmail.com>
2024-02-26 15:30:37 -0600

    Formateo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-const toggleBtn = document.querySelector('.toggle-btn')
-const toggleBtnIcon = document.querySelector('.toggle-btn i')
-const dropDownMenu = document.querySelector('.dropdown-menu')
+// const toggleBtn = document.querySelector('.menu-movil')
+// const toggleBtnIcon = document.querySelector('.toggle-btn')
+// const dropDownMenu = document.querySelector('..menu-movil__dropdown')
 
-toggleBtn.onclick = function(){
-  dropDownMenu.classList.toggle('open')
-  const isOpen = dropDownMenu.classList.contains('open')
+// toggleBtn.onclick = function(){
+//   dropDownMenu.classList.toggle('open')
+//   const isOpen = dropDownMenu.classList.contains('open')
 
-  toggleBtnIcon.classList = isOpen
-  ? 'fas fa-xmark'
-  : 'fas fa-bars'
-}
+//   toggleBtnIcon.classList = isOpen
+//   ? 'fas fa-xmark'
+//   : 'fas fa-bars'
+// }
