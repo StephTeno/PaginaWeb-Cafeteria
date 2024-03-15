866bc4e3732017b7d5594afd1501604c144ede17
StephTeno <StephanieTenorioCarolinaOrozco@gmail.com>
2024-03-15 14:35:05 -0600

    navbar responsive

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-// const toggleBtn = document.querySelector('.menu-movil')
-// const toggleBtnIcon = document.querySelector('.toggle-btn')
-// const dropDownMenu = document.querySelector('..menu-movil__dropdown')
+/* Hacer la responsividad del NavBar*/
+const toggleBtn = document.querySelector('.toggle-btn');
+const toggleBtnIcon = document.querySelector('.toggle-btn i');
+const dropdown = document.querySelector('.hero__navBar__dropdown');
 
-// toggleBtn.onclick = function(){
-//   dropDownMenu.classList.toggle('open')
-//   const isOpen = dropDownMenu.classList.contains('open')
+toggleBtn.addEventListener("click", function(){
+    dropdown.classList.toggle('open');
+    const isOpen = dropdown.classList.contains('open');
 
-//   toggleBtnIcon.classList = isOpen
-//   ? 'fas fa-xmark'
-//   : 'fas fa-bars'
-// }
+    toggleBtnIcon.classList = isOpen
+    ? 'fas fa-xmark'
+    : 'fas fa-bars'
+})
